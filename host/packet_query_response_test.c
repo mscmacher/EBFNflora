97b80eaea2c2e6f9a37b1db504f088395337dfbc
bong <mscmacher@gmail.com>
2018-03-13 22:54:53 +0100

    pacchetti di configurazione per ogni sensore

--- host/packet_query_response_test.c
+++ host/packet_query_response_test.c
@@ -8,8 +8,11 @@
 #include <unistd.h>
 
 #define STR_LENGTH 16
+char inputString[STR_LENGTH];
 
 PacketHandler packet_handler;
+
+/* -------------------- TimerConfigPacket -------------------- */
 
 #pragma pack(push, 1)
 typedef struct TimerConfigPacket{
@@ -30,7 +33,7 @@ PacketHeader* TimerConfigPacket_initializeBuffer(PacketType type, PacketSize siz
 }
 
 PacketOperations TimerConfigPacket_ops = {
-	0,
+	TIMER_CONFIG_PACKET_TYPE,
 	sizeof(TimerConfigPacket),
 	TimerConfigPacket_initializeBuffer,
 	0,
@@ -38,7 +41,111 @@ PacketOperations TimerConfigPacket_ops = {
 	0
 };
 
+/* -------------------- LightConfigPacket -------------------- */
+
+#pragma pack(push, 1)
+typedef struct LightConfigPacket{
+	PacketHeader header;
+	uint16_t minLight;
+	uint16_t maxLight;
+} LightConfigPacket;
+#pragma pack(pop)
+
+#define LIGHT_CONFIG_PACKET_TYPE 1
+#define LIGHT_CONFIG_PACKET_SIZE (sizeof(LightConfigPacket))
+
+LightConfigPacket light_config_packet_buffer;
+
+PacketHeader* LightConfigPacket_initializeBuffer(PacketType type, PacketSize size, void* args __attribute__((unused))) {
+	if (type != LIGHT_CONFIG_PACKET_TYPE || size != LIGHT_CONFIG_PACKET_SIZE)
+		return 0;
+	return (PacketHeader*) &light_config_packet_buffer;
+}
+
+PacketOperations LightConfigPacket_ops = {
+	LIGHT_CONFIG_PACKET_TYPE,
+	sizeof(LightConfigPacket),
+	LightConfigPacket_initializeBuffer,
+	0,
+	0,
+	0
+};
+
+/* -------------------- TemperatureConfigPacket -------------------- */
+
+#pragma pack(push, 1)
+typedef struct TemperatureConfigPacket{
+	PacketHeader header;
+	uint16_t minTemp;
+	uint16_t maxTemp;
+	uint32_t R1;
+} TemperatureConfigPacket;
+#pragma pack(pop)
+
+#define TEMPERATURE_CONFIG_PACKET_TYPE 2
+#define TEMPERATURE_CONFIG_PACKET_SIZE (sizeof(TemperatureConfigPacket))
+
+TemperatureConfigPacket temperature_config_packet_buffer;
+
+PacketHeader* TemperatureConfigPacket_initializeBuffer(PacketType type, PacketSize size, void* args __attribute__((unused))) {
+	if (type != TEMPERATURE_CONFIG_PACKET_TYPE || size != TEMPERATURE_CONFIG_PACKET_SIZE)
+		return 0;
+	return (PacketHeader*) &temperature_config_packet_buffer;
+}
+
+PacketOperations TemperatureConfigPacket_ops = {
+	TEMPERATURE_CONFIG_PACKET_TYPE,
+	sizeof(TemperatureConfigPacket),
+	TemperatureConfigPacket_initializeBuffer,
+	0,
+	0,
+	0
+};
+
+/* -------------------- PollutionConfigPacket -------------------- */
+
+#pragma pack(push, 1)
+typedef struct PollutionConfigPacket{
+	PacketHeader header;
+	uint16_t maxPoll;
+} PollutionConfigPacket;
+#pragma pack(pop)
+
+#define POLLUTION_CONFIG_PACKET_TYPE 3
+#define POLLUTION_CONFIG_PACKET_SIZE (sizeof(PollutionConfigPacket))
+
+PollutionConfigPacket pollution_config_packet_buffer;
+
+PacketHeader* PollutionConfigPacket_initializeBuffer(PacketType type, PacketSize size, void* args __attribute__((unused))) {
+	if (type != POLLUTION_CONFIG_PACKET_TYPE || size != POLLUTION_CONFIG_PACKET_SIZE)
+		return 0;
+	return (PacketHeader*) &pollution_config_packet_buffer;
+}
+
+PacketOperations PollutionConfigPacket_ops = {
+	POLLUTION_CONFIG_PACKET_TYPE,
+	sizeof(PollutionConfigPacket),
+	PollutionConfigPacket_initializeBuffer,
+	0,
+	0,
+	0
+};
+
+void read_from_stdin(void){
+	fgets(inputString, STR_LENGTH, stdin);
+	if((strlen(inputString) > 0) && (inputString[strlen(inputString) - 1] == '\n'))
+		inputString[strlen(inputString) - 1] = '\0';	
+}
+
 struct UART* uart;
+
+void flushOutputBuffer(int fd){
+	while (packet_handler.tx_size){
+		uint8_t c = PacketHandler_txByte(&packet_handler);
+		write(fd, &c, 1);
+		usleep(1000);
+	}
+}
 
 int main(int argc, char** argv){
 	assert(argc > 1);
@@ -50,30 +157,64 @@ int main(int argc, char** argv){
 	serial_set_blocking(fd, 1); 
 	if  (! fd)
 		return 0;
-		
-	char inputString[STR_LENGTH];
 	
 	PacketHandler_initialize(&packet_handler);
 	PacketHandler_installPacket(&packet_handler, &TimerConfigPacket_ops);
+	PacketHandler_installPacket(&packet_handler, &LightConfigPacket_ops);
+	PacketHandler_installPacket(&packet_handler, &TemperatureConfigPacket_ops);
+	PacketHandler_installPacket(&packet_handler, &PollutionConfigPacket_ops);
 	
-	while(1){
-		//getting timer duration from stdin
-		printf("Insert timer duration: ");
-		fgets(inputString, STR_LENGTH, stdin);
-		if((strlen(inputString) > 0) && (inputString[strlen(inputString) - 1] == '\n'))
-			inputString[strlen(inputString) - 1] = '\0';
+	/* -------------------- TimerConfigPacket -------------------- */
+	
+	printf("Insert timer duration: ");
+	read_from_stdin();
+	uint32_t duration = atoi(inputString);
+	
+	TimerConfigPacket p0 = { {TIMER_CONFIG_PACKET_TYPE, TIMER_CONFIG_PACKET_SIZE, 0}, duration };
+	PacketHandler_sendPacket(&packet_handler, (PacketHeader*) &p0);
+	flushOutputBuffer(fd);
+	
+	/* -------------------- LightConfigPacket -------------------- */
+
+	printf("Insert min light: ");
+	read_from_stdin();
+	uint16_t minLight = atoi(inputString);
+	
+	printf("Insert max light: ");
+	read_from_stdin();
+	uint16_t maxLight = atoi(inputString);
+	
+	LightConfigPacket p1 = { {LIGHT_CONFIG_PACKET_TYPE, LIGHT_CONFIG_PACKET_SIZE, 0}, minLight, maxLight };
+	PacketHandler_sendPacket(&packet_handler, (PacketHeader*) &p1);
+	flushOutputBuffer(fd);
+	
+	/* -------------------- TemperatureConfigPacket -------------------- */
+
+	printf("Insert min temperature: ");
+	read_from_stdin();
+	uint16_t minTemp = atoi(inputString);
+	
+	printf("Insert max temperature: ");
+	read_from_stdin();
+	uint16_t maxTemp = atoi(inputString);
+
+	printf("Insert thermistor resistance: ");
+	read_from_stdin();
+	uint32_t R1 = atoi(inputString);
+	
+	TemperatureConfigPacket p2 = { {TEMPERATURE_CONFIG_PACKET_TYPE, TEMPERATURE_CONFIG_PACKET_SIZE, 0}, minTemp, maxTemp, R1 };
+	PacketHandler_sendPacket(&packet_handler, (PacketHeader*) &p2);
+	flushOutputBuffer(fd);
+	
+	/* -------------------- PollutionConfigPacket -------------------- */
+	
+	printf("Insert max pollution: ");
+	read_from_stdin();
+	uint16_t maxPoll = atoi(inputString);
+	
+	PollutionConfigPacket p3 = { {POLLUTION_CONFIG_PACKET_TYPE, POLLUTION_CONFIG_PACKET_SIZE, 0}, maxPoll };
+	PacketHandler_sendPacket(&packet_handler, (PacketHeader*) &p3);
+	flushOutputBuffer(fd);
 		
-		uint32_t duration = atoi(inputString);
-		TimerConfigPacket p0 = { {TIMER_CONFIG_PACKET_TYPE, TIMER_CONFIG_PACKET_SIZE, 0}, duration };
-		
-		//sending to MCU
-		PacketHandler_sendPacket(&packet_handler, (PacketHeader*) &p0);
-		while (packet_handler.tx_size){
-			uint8_t c = PacketHandler_txByte(&packet_handler);
-			write(fd, &c, 1);
-			usleep(1000);
-		}
-		
-		break;
-	}
+	return 0;
 }
--- micro/packet_query_response_test.c
+++ micro/packet_query_response_test.c
@@ -8,6 +8,8 @@
 
 PacketHandler packet_handler;
 
+/* -------------------- TimerConfigPacket -------------------- */
+
 #pragma pack(push, 1)
 typedef struct TimerConfigPacket{
 	PacketHeader header;
@@ -37,11 +39,137 @@ PacketStatus TimerConfigPacket_onReceive(PacketHeader* header, void* args __attr
 }
 
 PacketOperations TimerConfigPacket_ops = {
-	0,
+	TIMER_CONFIG_PACKET_TYPE,
 	sizeof(TimerConfigPacket),
 	TimerConfigPacket_initializeBuffer,
 	0,
 	TimerConfigPacket_onReceive,
+	0
+};
+
+/* -------------------- LightConfigPacket -------------------- */
+
+#pragma pack(push, 1)
+typedef struct LightConfigPacket{
+	PacketHeader header;
+	uint16_t minLight;
+	uint16_t maxLight;
+} LightConfigPacket;
+#pragma pack(pop)
+
+#define LIGHT_CONFIG_PACKET_TYPE 1
+#define LIGHT_CONFIG_PACKET_SIZE (sizeof(LightConfigPacket))
+
+LightConfigPacket light_config_packet_buffer;
+
+PacketHeader* LightConfigPacket_initializeBuffer(PacketType type, PacketSize size, void* args __attribute__((unused))) {
+	if (type != LIGHT_CONFIG_PACKET_TYPE || size != LIGHT_CONFIG_PACKET_SIZE)
+		return 0;
+	return (PacketHeader*) &light_config_packet_buffer;
+}
+
+PacketStatus LightConfigPacket_onReceive(PacketHeader* header, void* args __attribute__((unused))){
+	LightConfigPacket* p = (LightConfigPacket*) header;
+	int minLight = p->minLight;
+	int maxLight = p->maxLight;
+	char buffer[16];
+	sprintf(buffer, "%d, %d", minLight, maxLight);
+	LCDclr();
+	LCDstring((uint8_t*)buffer, strlen(buffer));
+	return Success;
+}
+
+PacketOperations LightConfigPacket_ops = {
+	LIGHT_CONFIG_PACKET_TYPE,
+	sizeof(LightConfigPacket),
+	LightConfigPacket_initializeBuffer,
+	0,
+	LightConfigPacket_onReceive,
+	0
+};
+
+/* -------------------- TemperatureConfigPacket -------------------- */
+
+#pragma pack(push, 1)
+typedef struct TemperatureConfigPacket{
+	PacketHeader header;
+	uint16_t minTemp;
+	uint16_t maxTemp;
+	uint32_t R1;
+} TemperatureConfigPacket;
+#pragma pack(pop)
+
+#define TEMPERATURE_CONFIG_PACKET_TYPE 2
+#define TEMPERATURE_CONFIG_PACKET_SIZE (sizeof(TemperatureConfigPacket))
+
+TemperatureConfigPacket temperature_config_packet_buffer;
+
+PacketHeader* TemperatureConfigPacket_initializeBuffer(PacketType type, PacketSize size, void* args __attribute__((unused))) {
+	if (type != TEMPERATURE_CONFIG_PACKET_TYPE || size != TEMPERATURE_CONFIG_PACKET_SIZE)
+		return 0;
+	return (PacketHeader*) &temperature_config_packet_buffer;
+}
+
+PacketStatus TemperatureConfigPacket_onReceive(PacketHeader* header, void* args __attribute__((unused))){
+	TemperatureConfigPacket* p = (TemperatureConfigPacket*) header;
+	int minTemp = p->minTemp;
+	int maxTemp = p->maxTemp;
+	int R1 = p->R1;
+	char buffer[16];
+	sprintf(buffer, "%d, %d", minTemp, maxTemp);
+	LCDclr();
+	LCDstring((uint8_t*)buffer, strlen(buffer));
+	sprintf(buffer, "%d", R1);
+	LCDGotoXY(0,1);
+	LCDstring((uint8_t*)buffer, strlen(buffer));
+	return Success;
+}
+
+PacketOperations TemperatureConfigPacket_ops = {
+	TEMPERATURE_CONFIG_PACKET_TYPE,
+	sizeof(TemperatureConfigPacket),
+	TemperatureConfigPacket_initializeBuffer,
+	0,
+	TemperatureConfigPacket_onReceive,
+	0
+};
+
+/* -------------------- PollutionConfigPacket -------------------- */
+
+#pragma pack(push, 1)
+typedef struct PollutionConfigPacket{
+	PacketHeader header;
+	uint16_t maxPoll;
+} PollutionConfigPacket;
+#pragma pack(pop)
+
+#define POLLUTION_CONFIG_PACKET_TYPE 3
+#define POLLUTION_CONFIG_PACKET_SIZE (sizeof(PollutionConfigPacket))
+
+PollutionConfigPacket pollution_config_packet_buffer;
+
+PacketHeader* PollutionConfigPacket_initializeBuffer(PacketType type, PacketSize size, void* args __attribute__((unused))) {
+	if (type != POLLUTION_CONFIG_PACKET_TYPE || size != POLLUTION_CONFIG_PACKET_SIZE)
+		return 0;
+	return (PacketHeader*) &pollution_config_packet_buffer;
+}
+
+PacketStatus PollutionConfigPacket_onReceive(PacketHeader* header, void* args __attribute__((unused))){
+	PollutionConfigPacket* p = (PollutionConfigPacket*) header;
+	int maxPoll = p->maxPoll;
+	char buffer[8];
+	sprintf(buffer, "%d", maxPoll);
+	LCDclr();
+	LCDstring((uint8_t*)buffer, strlen(buffer));
+	return Success;
+}
+
+PacketOperations PollutionConfigPacket_ops = {
+	POLLUTION_CONFIG_PACKET_TYPE,
+	sizeof(PollutionConfigPacket),
+	PollutionConfigPacket_initializeBuffer,
+	0,
+	PollutionConfigPacket_onReceive,
 	0
 };
 
@@ -54,7 +182,6 @@ void flushInputBuffers(void){
 	}
 }
 
-
 int main(int argc, char** argv){
 	uart = UART_init(0, 115200);
 	
@@ -64,6 +191,9 @@ int main(int argc, char** argv){
 	
 	PacketHandler_initialize(&packet_handler);
 	PacketHandler_installPacket(&packet_handler, &TimerConfigPacket_ops);
+	PacketHandler_installPacket(&packet_handler, &LightConfigPacket_ops);
+	PacketHandler_installPacket(&packet_handler, &TemperatureConfigPacket_ops);
+	PacketHandler_installPacket(&packet_handler, &PollutionConfigPacket_ops);
 
 	while (1){
 		flushInputBuffers();
